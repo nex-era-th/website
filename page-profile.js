ee29e5cff51288316ae558e0b103e901b1c5f013
mutita <mutita.org@gmail.com>
2025-04-15 16:26:30 +0700

    added search items to the search-box

---
+++ page-profile.js
@@ -0,0 +1,33 @@
+// page-profile.js
+// for : search through the site by tags
+
+var nexWebPageProfile = [
+  { 
+    url: '/about.html', 
+    tag: 'about nex-era advisory software tech ซอฟท์แวร์ ที่ปรึกษา เทค เทคโนโลยี เกี่ยวกับ',
+  },
+  {
+    url: '/advisory.html',
+    tag: 'advisory leadership governance management strategy tech buddhism modern life ที่ปรึกษา ความเป็นผู้นำ ปกครอง บริหาร ยุทธศาสตร์ เทคโนโลยี ธรรม ชีวิต'
+  },
+  {
+    url: '/software.html',
+    tag: 'software design ui dev develop tech support open source web app mobile app ai iot ซอฟท์แวร์ ออกแบบ เขียนโปรแกรม business app application sales purchase inventory warehouse production hr human resources mrp crm report node nodejs js java script linux database visual studio web browser'
+  },
+  {
+    url: '/technology.html',
+    tag: 'tech technology บริหารเทคโนโลยี ให้คำปรึกษาด้านเทคโนโลยี'
+  },
+  {
+    url: '/project.html',
+    tag: 'project-x projex nex-biz nex-social nex-money nex-msg nex-gov โครงการ'
+  },
+  {
+    url: '/update.html',
+    tag: 'update ข่าวสาร tech software advisory lisa politic world'
+  },
+  {
+    url: '/advisory-sect/what-is-strategy.html',
+    tag: 'strategy ยุทธศาสตร์ กลยุทธ์'
+  }
+]
