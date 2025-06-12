67b99729b213eab8d600e7b20d7dfef4a64f284b
mutita <mutita.org@gmail.com>
2025-06-12 22:05:41 +0700

    improved UI; adjusted font-size in table; show local time in dev-profile-info.html

---
+++ devsk/config.js
@@ -0,0 +1,14 @@
+/*
+
+for: config file for /devsk/index.html
+var: conf
+
+*/
+
+
+const conf = {
+  apiUrl        : 'https://node-x-qd6s.onrender.com', 
+  versionDisplay: '1.2.4',
+  //apiUrl        : 'http://192.168.1.111:10000',
+  //versionDisplay: '1.3*'
+}
