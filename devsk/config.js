d75a8a6241d743efe2f6fd6e08d1c9943482f2bc
mutita <mutita.org@gmail.com>
2025-06-13 08:53:44 +0700

    changed config.js to real server.

--- devsk/config.js
+++ devsk/config.js
@@ -7,15 +7,15 @@ var: conf
 
 
 const conf = {
-  
+
   key: 'c3f1f195dbb658ac77f2277e12c9d82ab571ad99d1d770b4cba804ec8fde2955',
   salt: 'DJo4C-QNU~C8',
 
   //real server
-  //apiUrl        : 'https://node-x-qd6s.onrender.com', 
-  //versionDisplay: '&beta;',
+  apiUrl        : 'https://node-x-qd6s.onrender.com', 
+  versionDisplay: '&beta;',
   
   //test server
-  apiUrl        : 'http://192.168.1.111:10000',
-  versionDisplay: '&beta;'
+  //apiUrl        : 'http://192.168.1.111:10000',
+  //versionDisplay: '&beta;'
 }
