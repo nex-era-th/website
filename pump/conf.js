a8e86129de2d3e6dc1b24e9a699cfddf4455a21b
mutita <mutita.org@gmail.com>
2025-12-21 08:18:11 +0700

    stable; added Find Product page

--- pump/conf.js
+++ pump/conf.js
@@ -1,5 +1,15 @@
+
+
+//////////////////////////////////////////////////
+//                                              //
+//    CONFIG FILE FOR PUMP PROJECT (BROWSER)    //
+//                                              //
+//////////////////////////////////////////////////
+
+
+const MASTER_KEY = '7d4128e078f09aed78d3dadc987f22c6c5a2065aca5b3f8a6690c32808e9e6db'
+
 const PUMP_DOMAIN = 'https://pump-1ati.onrender.com'
 //const PUMP_DOMAIN = 'http://192.168.1.111:4000'
 
-const MASTER_KEY = '7d4128e078f09aed78d3dadc987f22c6c5a2065aca5b3f8a6690c32808e9e6db'
 
