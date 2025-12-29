1f18d3333be8a3543cd351b2d704b12f2a523de4
mutita <mutita.org@gmail.com>
2025-12-29 08:19:41 +0700

    stable; update /pump/conf.js

--- pump/conf.js
+++ pump/conf.js
@@ -14,8 +14,8 @@ const MASTER_KEY = '7d4128e078f09aed78d3dadc987f22c6c5a2065aca5b3f8a6690c32808e9
 
 
 
-//const PUMP_DOMAIN = 'https://pump-1ati.onrender.com'
-const PUMP_DOMAIN = 'http://localhost:4000'
+const PUMP_DOMAIN = 'https://pump-1ati.onrender.com'
+//const PUMP_DOMAIN = 'http://localhost:4000'
 
 
 
