e1e0f63656d8493df5560fed56aec3ea78816295
mutita <mutita.org@gmail.com>
2026-01-25 09:09:33 +0700

    pump/conf.js ~ make it more smarter by checking the location.host itself. No manual setting any more;

--- pump/conf.js
+++ pump/conf.js
@@ -7,15 +7,16 @@
 //////////////////////////////////////////////////
 
 
-//const PUMP_DOMAIN = 'http://192.168.1.111:4000'
-
 
 const MASTER_KEY = '7d4128e078f09aed78d3dadc987f22c6c5a2065aca5b3f8a6690c32808e9e6db'
 
 
-
-//const PUMP_DOMAIN = 'https://pump-1ati.onrender.com'
-const PUMP_DOMAIN = 'http://localhost:4000'
+let PUMP_DOMAIN;
+if (location.host.includes('nex-era.xyz')) {
+  PUMP_DOMAIN = 'https://pump-1ati.onrender.com'
+} else {
+  PUMP_DOMAIN = 'http://localhost:4000'
+}
 
 
 
