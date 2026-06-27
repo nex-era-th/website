789f13f01cb8d90750922f8dbbaabcc8f5492c39
mutita <mutita.org@gmail.com>
2026-06-27 19:26:41 +0700

    fixed typos, refine words, and few == ok

---
+++ emplio-user-guide/video-list-data.js
@@ -0,0 +1,10 @@
+/*
+      put video id in this var
+*/
+
+
+const videoListData = [
+  { title: "เอ็มพลิโอ360 - แนะนำตัว", link: "F7k3tjHHXO8" },
+  { title: "living in the next #styl", link: "-juu8rfPazY"}
+  // Simply add more objects here when you have more videos
+];
